9921f4dfda260bedf6740b660704df0eacdc7d51
João Farias <jgfd@cin.ufpe.br>
2019-03-09 15:40:32 +0100

    CallDriver Test - Implementing Call Driver Flow

---
+++ AndroidDemo/app/src/androidTest/java/com/mytaxi/android_demo/CallDriverTest.java
@@ -0,0 +1,60 @@
+package com.mytaxi.android_demo;
+
+import android.support.test.rule.ActivityTestRule;
+import android.support.test.rule.GrantPermissionRule;
+import android.support.test.runner.AndroidJUnit4;
+
+import com.mytaxi.android_demo.activities.MainActivity;
+import com.mytaxi.android_demo.baseTests.AuthenticationActivityTest;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.action.ViewActions.typeText;
+import static android.support.test.espresso.assertion.ViewAssertions.doesNotExist;
+
+/**
+ * Test class to validate myTaxi demo login process
+ */
+@RunWith(AndroidJUnit4.class)
+public class CallDriverTest extends AuthenticationActivityTest {
+
+    private final String username = "crazydog335";
+    private final String password = "venture";
+
+    @Rule
+    public ActivityTestRule<MainActivity> activityRule =
+            new ActivityTestRule<>(MainActivity.class);
+
+    @Rule
+    public GrantPermissionRule permissionRule = GrantPermissionRule.grant(android.Manifest.permission.ACCESS_FINE_LOCATION);
+
+    @Test
+    public void checkCallDriverFlow() {
+        this.getUsernameInput().perform(typeText(username));
+        this.getPasswordInput().perform(typeText(password));
+        login();
+
+        this.getSearchInput().perform(typeText("sa"));
+
+        this.selectDriverNamed("Sarah Scott", activityRule);
+
+        this.callDriver();
+
+        // TODO - Stub ACTION_DIAL Intent
+    }
+
+    private void login() {
+        this.getLoginButton().perform(click());
+
+        // TODO - To be replaced with IdlingResource
+        try {
+            Thread.sleep(2000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        this.getUsernameInput().check(doesNotExist());
+    }
+}
--- AndroidDemo/app/src/androidTest/java/com/mytaxi/android_demo/screenTests/AuthenticationScreenTest.java
+++ AndroidDemo/app/src/androidTest/java/com/mytaxi/android_demo/screenTests/AuthenticationScreenTest.java
@@ -1,4 +1,4 @@
-package com.mytaxi.android_demo;
+package com.mytaxi.android_demo.screenTests;
 
 import android.content.Context;
 import android.support.test.InstrumentationRegistry;
@@ -6,47 +6,45 @@ import android.support.test.espresso.ViewInteraction;
 import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
 
+import com.mytaxi.android_demo.R;
 import com.mytaxi.android_demo.activities.AuthenticatedActivity;
 import com.mytaxi.android_demo.activities.AuthenticationActivity;
+import com.mytaxi.android_demo.baseTests.AuthenticationActivityTest;
 
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
-import static android.support.test.espresso.Espresso.onView;
 import static android.support.test.espresso.action.ViewActions.typeText;
 import static android.support.test.espresso.assertion.ViewAssertions.matches;
-
-import static android.support.test.espresso.matcher.ViewMatchers.withId;
 import static android.support.test.espresso.matcher.ViewMatchers.withText;
 
 /**
  * Test class to validate the UI and behavior of the Authetication screen
  */
 @RunWith(AndroidJUnit4.class)
-public class AuthenticationScreenTest {
+public class AuthenticationScreenTest extends AuthenticationActivityTest {
 
+    private final Context appContext = InstrumentationRegistry.getTargetContext();
     @Rule
     public ActivityTestRule<AuthenticatedActivity> authenticationScreen = new ActivityTestRule(AuthenticationActivity.class);
 
-    private final Context appContext = InstrumentationRegistry.getTargetContext();
-
     @Test
-    public void checkLoginButtonText(){
+    public void checkLoginButtonText() {
         String expectedLoginButtonText = appContext.getString(R.string.button_login);
-        ViewInteraction loginButton = onView(withId(R.id.btn_login));
+        ViewInteraction loginButton = this.getLoginButton();
         loginButton.check(matches(withText(expectedLoginButtonText)));
     }
 
     @Test
-    public void checkUsernameButtonText(){
-        ViewInteraction usernameInput = onView(withId(R.id.edt_username));
+    public void checkUsernameButtonText() {
+        ViewInteraction usernameInput = this.getUsernameInput();
         inputValueAndCheckText(usernameInput, "crazydog335");
     }
 
     @Test
-    public void checkPasswordButtonText(){
-        ViewInteraction passwordInput = onView(withId(R.id.edt_password));
+    public void checkPasswordButtonText() {
+        ViewInteraction passwordInput = this.getPasswordInput();
         inputValueAndCheckText(passwordInput, "123456");
     }
 
